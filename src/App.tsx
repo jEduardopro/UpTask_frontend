bc70f4d4e8b94d08b95e2e0b145e5b0cfecd098f
Jesus Eduardo <lalo.codein22@gmail.com>
2023-09-29 07:49:23 -0600

    define routes and pages of authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import AuthLayout from './layouts/AuthLayout'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import ForgotPassword from './pages/ForgotPassword'
+import ResetPassword from './pages/ResetPassword'
+import ConfirmAccount from './pages/ConfirmAccount'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+			<Routes>
+				<Route path="/" element={<AuthLayout />} >
+					<Route index element={<Login />} />
+					<Route path='register' element={<Register />} />
+					<Route path='forgot-password' element={<ForgotPassword />} />
+					<Route path='forgot-password/:token' element={<ResetPassword />} />
+					<Route path='confirm-account/:id' element={<ConfirmAccount />} />
+				</Route>
+				<Route path="*" element={<h1>Not Found</h1>} />
+			</Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+const AuthLayout = () => {
+	return (
+		<>
+			Auth layout
+			<Outlet />
+		</>
+	)
+}
+
+export default AuthLayout
